2a1af60e66527275977ffad1cac62b40ead6ffeb
Tsagkaropoulos Angelo <ang.tsagkaropoulos@cz.lan>
2026-01-30 09:46:42 +0100

    .

--- Es29-01-2025 thread/src/MyFrame.java
+++ Es29-01-2025 thread/src/MyFrame.java
@@ -1,12 +1,43 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MyFrame extends JFrame {
 
+    private final BufferedImage tela;
+    private final Turtle turtle;
     JPanel p;
-    Turtle turtle;
+
+    private Color[] colors = {
+            Color.RED,
+            Color.ORANGE,
+            Color.YELLOW,
+            Color.GREEN,
+            Color.CYAN,
+            Color.BLUE,
+            Color.MAGENTA
+    };
 
 
     public MyFrame(){
+        tela = new BufferedImage(600,600,
+                BufferedImage.TYPE_INT_ARGB);
 
+        turtle = new Turtle(300,300,tela);
+
+        spirale(20, 25, 0);
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        g.drawImage(tela,0,0,null);
+    }
+
+    /*
+        Timer timer = new Timer(200, null);
+        timer.addActionListener(e ->{
+            timer.stop();
+        });
+    */
 }
--- Es29-01-2025 thread/src/Turtle.java
+++ Es29-01-2025 thread/src/Turtle.java
@@ -1,121 +1,67 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Turtle extends JPanel{
-    private int x;
-    private int y;
-    private int angle;
+    private double x, y;
+    private double angle;
     private boolean isPenDown;
-    private String color;
+    private Color color;
+    private final BufferedImage tela;
 
-    public Turtle(int x, int y, int angle, boolean isPenDown, String color) {
+    public Turtle(double x, double y, BufferedImage tela) {
         this.x = x;
         this.y = y;
-        this.angle = angle;
-        this.isPenDown = isPenDown;
+        this.angle = 0;
+        this.isPenDown = true;
         this.color = color;
+        this.tela = tela;
     }
 
     public void forward (int len){
-        double radians = (Math.PI / 180) * this.angle;
+        Graphics2D g = (Graphics2D) tela.getGraphics();
 
+        double radians = (Math.PI / 180) * this.angle;
         double deltaX = Math.cos(radians) * len;
         double deltaY = Math.sin(- radians) * len;
 
         double newX = this.x + deltaX;
         double newY = this.y + deltaY;
 
-        if (this.isPenDown){
-
-        }
-
-    }
-
-    public void paintComponent(Graphics g){
-        super.paintComponent(g); // va fatto sempre
-        setBackground(Color.white); // fondo bianco
-
-
-        g.setColor(Color.black);
-        g.setColor(Color.red);
-        g.drawLine(0,10/2, 10-1,10/2);
-        g.drawLine(10/2,0,10/2,10-1);
-        g.drawString(""+10, 5,10/2-5);
-        g.drawString(""+10, 10-10,10/2-5);
-        g.drawString(""+10, 10/2+5,15);
-        g.drawString(""+10, 10/2+5,10-5);
-        g.setColor(Color.blue);
-        setPixel(g,10,f(10));
-        for (int ix=1; ix<10; ix++){
-            float x = 10+((float)ix)/10;
-            setPixel(g,x,f(x));
-            g.dispose();
-        }
-    }
-
-    static float f(float x){
-        return (float)Math.sin(x);
-    }
-
-    void setPixel(Graphics g, float x, float y){
-        if (x<10 || x>10 || y<10 || y>10 )
-            return;
-        int ix = Math.round((x-10)*10);
-        int iy = 10-Math.round(
-                (y-10)*10);
-        g.drawLine(ix,iy,ix,iy);
-        Timer timer = new Timer(200, null);
-        timer.addActionListener(e ->{
-            timer.stop();
-        });
-    }
-}
-
-/*
-* const turtle = {
-    x: window.innerWidth /2,
-    y: window-innerHeight /2,
-    angle: 0,
-    isPenDown : true,
-    color:"#000000",
-
-    forward: function(distance) {
-
         if (this.isPenDown) {
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(newX, newY);
-            ctx.strokeStyle = this.color;
-            ctx.stroke();
+            g.setColor(this.color);
+            g.drawLine((int) x, (int) y, (int) newX, (int) newY);
         }
         this.x = newX;
         this.y = newY;
-    },
 
-    turn: function (a){
+        g.dispose();
+    }
+
+    public void turn (double a){
         this.angle += a;
         if (this.angle >= 360) {
             this.angle -= 360;
         }
-    },
+    }
 
-    penUp: function (){
+    public void penUp(){
         this.isPenDown = false;
-    },
+    }
 
-    penDown: function (){
+    public void penDown(){
         this.isPenDown = true;
-    },
+    }
 
-    changeColor: function (color){
+    public void changeColor(Color color){
         this.color = color;
-    },
+    }
 
-    setX: function (x){
+    public void setX(double x){
         this.x = x;
-    },
+    }
 
-    setY: function (y){
+    public void setY(double y){
         this.y = y;
-    },
-};*/
+    }
+}
